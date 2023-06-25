6ed72f28a830c047f72c6178c52ae0780c9a9481
fede-sol <fedesolanes1@gmail.com>
2023-06-25 18:08:45 -0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,10 @@
 package main;
 
 
-import reservas.Reserva;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("asd");
-        Reserva r= new Reserva();
+        
+       
     }
 }
