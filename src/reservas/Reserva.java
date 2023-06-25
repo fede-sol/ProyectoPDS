e7e0420178e3232838228d0adf43f638a76894f7
fede-sol <fedesolanes1@gmail.com>
2023-06-25 16:48:40 -0300

    Las variables ahora son privadas, se agregaron getters y setters

--- src/reservas/Reserva.java
+++ src/reservas/Reserva.java
@@ -8,14 +8,19 @@ import java.util.List;
 import pagos.IMedioPago;
 
 public class Reserva {
-    LocalDateTime fechaCreacion;
-    Cliente cliente;
-    List<Huesped> huespedes;
-    IMedioPago medioPago;
-    IPoliticaReserva politicaReserva;
-    EstadoReserva estadoReserva;
-    Habitacion habitacion;
-    Disponibilidad disponibilidad;
+    
+    private LocalDateTime fechaCreacion;
+    private Cliente cliente;
+    private List<Huesped> huespedes;
+    private IMedioPago medioPago;
+    private IPoliticaReserva politicaReserva;
+    private EstadoReserva estadoReserva;
+    private Habitacion habitacion;
+    private Disponibilidad disponibilidad;
+    
+
+    
+    
 
     public double calcularMonto() {
         return habitacion.calcularPrecio()-(1-politicaReserva.getPorcentaje()/100);
@@ -41,4 +46,64 @@ public class Reserva {
         estadoReserva.cancelar();
     }
 
+    
+    
+    
+    
+    /*
+     * Sección de getters y setters
+     */
+    
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Huesped> getHuespedes() {
+        return huespedes;
+    }
+
+    public void setHuespedes(List<Huesped> huespedes) {
+        this.huespedes = huespedes;
+    }
+
+    public EstadoReserva getEstadoReserva() {
+        return estadoReserva;
+    }
+
+    public void setEstadoReserva(EstadoReserva estadoReserva) {
+        this.estadoReserva = estadoReserva;
+    }
+
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
+
+    public Disponibilidad getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(Disponibilidad disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+    
+    
+    
+    
+    
 }
